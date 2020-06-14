12ea20368f803e275662ec9dd0ee96884c475ec8
timwtoy <timwtoy@gmail.com>
2020-06-14 17:28:20 -0500

    removing unused code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,7 +24,6 @@ export class HomeComponent implements OnInit {
 
   dataSource: MatTableDataSource<TimeOfDay> = new MatTableDataSource<TimeOfDay>();
   displayedColumns = ['formattedDisplay', 'slotsOpen'];
-  expandedElement: PeriodicElement | null;
   timeOptions = [
     {id: 0, label: '60 Minutes'},
     {id: 1, label: '90 Minutes'}
@@ -138,23 +137,3 @@ export class HomeComponent implements OnInit {
     return `${numberToBeFormatted}${minutes} ${meridiem}`;
   }
 }
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
